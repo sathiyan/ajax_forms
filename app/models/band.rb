8fadb10b8ae09cc8b5793c443dc99378602b68d0
Ed James <ed.james.email@gmail.com>
2011-05-23 10:27:36 +0100

    Clean up!

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,5 @@
 class Band < ActiveRecord::Base
   attr_accessible :name
-
+  
   validates :name, :uniqueness => true, :presence => true
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('.edit_band').live('click', function(){
+  $('a.edit_band').live('click', function() {
     tr = $(this).closest('tr').toggle();
     tr_form = tr.next('tr').toggle();
-    tr_form.find('form')[0].reset();
+//    tr_form.find('form')[0].reset();
     return false;
   });
 
-  $('.cancel_update').live('click', function(){
+  $('.cancel_update').live('click', function() {
     tr = $(this).closest('tr').toggle();
     tr_form = tr.prev('tr').toggle();
     return false;
   });
 
+  $("#new-label-form")
+      .bind("ajax:success", function(xhr, data, status) { console.log(data) })
+      .bind("ajax:error", function(xhr, data, status) { console.log(data) })
 });
